0d55108844371a5b352b8552714ae29804fd9b10
nurik_chargynov <t.a.chargynov.kg@gmail.com>
2026-08-26 19:47:10 +0600

    Add Firebase SDK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -7,10 +10,11 @@ plugins {
 
 android {
     namespace = "com.geeks.newsapp.news_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -41,4 +45,17 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Подключаем набор совместимых реализаций Java API для desugaring.
+    //
+    // Строка `isCoreLibraryDesugaringEnabled = true` только включает механизм,
+    // а эта зависимость добавляет саму библиотеку `desugar_jdk_libs`, где лежат
+    // реализации Java 8+ API для старых Android-версий.
+    //
+    // Проще: если плагин вызывает новый Java API, Android Gradle Plugin
+    // подставляет совместимую реализацию из `desugar_jdk_libs`, чтобы код
+    // работал не только на новых Android, но и на устройствах ниже нужного API.
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
